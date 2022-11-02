4cee4394a6876286f2b02b30bc5bf5da8f67afd3
inessitisarah <78847095+inessitisarah@users.noreply.github.com>
2022-11-02 13:56:54 +0700

    tugas day 7 CRUD (belum bisa update)

---
+++ Tugas Day 7/day-7/go.mod
@@ -0,0 +1,11 @@
+module mymodule
+
+go 1.16
+
+require (
+	github.com/gofiber/fiber/v2 v2.39.0
+	github.com/jinzhu/copier v0.3.5
+	github.com/joho/godotenv v1.4.0
+	gorm.io/driver/postgres v1.4.5
+	gorm.io/gorm v1.24.1-0.20221019064659-5dd2bb482755
+)
--- Tugas Day 7/day-7/main.go
+++ Tugas Day 7/day-7/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"mymodule/config"
+	"mymodule/handler"
+	"mymodule/repository"
+	"mymodule/routes"
+	"mymodule/usecase"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	config.Database()
+	config.AutoMigrate()
+
+	app := fiber.New()
+
+	userRepository := repository.NewUserRepository(config.DB)
+	userUsecase := usecase.NewUserUsecase(userRepository)
+	userHandler := handler.NewUserHandler(userUsecase)
+
+	routes.Routes(app, userHandler)
+
+	app.Listen(":3000")
+
+	//app := fiber.New()
+}
